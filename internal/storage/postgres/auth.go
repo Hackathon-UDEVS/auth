96a2c79e77d760cc2ba58e99cf8822d2e4b8b978
Avazbek-02 <avazbekmambetov9@gmail.com>
2024-12-03 23:14:21 +0500

    done

--- internal/storage/postgres/auth.go
+++ internal/storage/postgres/auth.go
@@ -64,7 +64,7 @@ func (s *UserRepo) Login(ctx context.Context, req *pb.LoginReq) (*pb.LoginRes, e
 		return nil, errors.New("invalid email or password")
 	}
 
-	// Return user data if login is successful
+	// Return user data if login is successful12
 	return &pb.LoginRes{UserRes: &user}, nil
 }
 
